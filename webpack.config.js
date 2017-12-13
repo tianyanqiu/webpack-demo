c89a310ababa487f08b5ebd8e8bab871b2339ea2
tianyanqiu <745916729@qq.com>
2017-12-13 16:45:10 +0800

    chore: 缓存

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    index: './src/index.js',
+    main: './src/index.js',
+    vendor: [
+      'lodash'
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HTMLWebpackPlugin({
-      title: 'Code Splitting'
+      title: 'Caching'
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     })
   ],
   output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   }
 };
