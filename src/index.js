41e85efc020704040cd58ef126ad0d13a79a6261
tianyanqiu <745916729@qq.com>
2017-12-13 18:00:07 +0800

    chore: 创建Library

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
- import _ from 'lodash';
-//  import Print from './print';
+import _ from 'lodash';
+import numRef from './ref.json';
 
- function component() {
-   var element = document.createElement('div');
+export function numToWord(num) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
+};
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    // element.onclick = Print.bind(null,'Hello webpack!')
-
-    return element;
-  }
-
-
- document.body.appendChild(component());
+export function wordToNum(word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,25 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: {
     main: './src/index.js',
-    vendor: [
-      'lodash'
-    ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HTMLWebpackPlugin({
-      title: 'Caching'
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime'
-    })
+    new CleanWebpackPlugin(['dist'])
   ],
   output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'webpack-numbers.js',
+    // path: path.resolve(__dirname, 'dist')
+    library: 'webpackNumbers',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: "_"
+    }
   }
 };
