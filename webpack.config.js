369643aaae1f1e300debd5aeb16008925f4eb1cb
tianyanqiu <tianyanqiu@sinosoft.com.cn>
2019-04-09 14:37:24 +0800

    chore:调整配置

--- webpack.config.js
+++ webpack.config.js
@@ -11,18 +11,25 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       title: 'Caching'
     }),
     new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime'
-    })
+    
   ],
+  optimization: {
+    splitChunks: {
+        cacheGroups: {
+            commons: {
+                name: "vendor",
+                chunks: "initial",
+                minChunks: 2
+            }
+        }
+    }
+},
+
   output: {
     filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
