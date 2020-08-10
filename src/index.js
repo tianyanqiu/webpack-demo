5f1cde32623ad4907007917c8b898ce866171e86
tianyanqiu <745916729@qq.com>
2020-08-10 10:46:09 +0800

    chore:HMR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import Print from "./print";
+import "./style.css";
 
 function component() {
   var element = document.createElement("div");
@@ -15,11 +16,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept("./print.js", function () {
     console.log("Accepting the updated printMe module!");
-    Print.bind(null, "Hello webpack!");
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
   });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,38 +15,42 @@ module.exports = {
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
-    },
+  // optimization: {
+  //   runtimeChunk: "single",
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: "vendors",
+  //         chunks: "all",
+  //       },
+  //     },
+  //   },
+  // },
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
   },
   output: {
     filename: "[name].boundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
-  // module: {
-  //   rules: [
-  //     { test: /\.css$/, use: ["style-loader", "css-loader"] },
-  //     {
-  //       test: /\.(png|svg|jpg|gif)$/,
-  //       use: ["file-loader"],
-  //     },
-  //     {
-  //       test: /\.(csv|tsv)$/,
-  //       use: ["csv-loader"],
-  //     },
-  //     {
-  //       test: /\.xml$/,
-  //       use: ["xml-loader"],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      //     {
+      //       test: /\.(png|svg|jpg|gif)$/,
+      //       use: ["file-loader"],
+      //     },
+      //     {
+      //       test: /\.(csv|tsv)$/,
+      //       use: ["csv-loader"],
+      //     },
+      //     {
+      //       test: /\.xml$/,
+      //       use: ["xml-loader"],
+      //     },
+    ],
+  },
 };
